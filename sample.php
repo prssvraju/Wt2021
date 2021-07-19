3f833dec0a9887b5771c338b5f6be94de81d26ab
prssvrajusrkr <prssvr@srkrec.ac.in>
2021-07-19 09:52:28 +0530

    arrays

---
+++ sample.php
@@ -0,0 +1,15 @@
+<?php
+    define("PI",3.14);
+    echo "value of pi".PI."<br>";
+
+    $num = array(1,2,3);
+    $names = array("1"=>"raghu","2"=>"ravi");
+    $mixed = array("color"=>"red","1"=>"blue",3=>"balck");
+
+    echo "Numaric(key) array is $num[2]<br>";
+    echo "Associative array is $names[1]<br>";
+    echo "Mixed array is $mixed[1]<br>";
+    echo "Mixed array is $mixed[color]<br>";
+    echo "Mixed array is $mixed[3]"
+    
+?>
